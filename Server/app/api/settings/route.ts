56b56ac6cba9d80e330e06b588980f57e282a844
Tim-M1 <93043257+Tim-M1@users.noreply.github.com>
2025-08-10 17:59:55 +1000

    refactor: outline additional routes
    33

---
+++ Server/app/api/settings/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() { }
+
+
+
+// get settings        
+// update settings
+
+// use single record with static ID
---
+++ Server/app/api/users/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() { }
+
+
+
+// Get all users        
+// Get specific user    userId
+// Create user          data
+// Delete user          userId
+// Update user          userId  data
