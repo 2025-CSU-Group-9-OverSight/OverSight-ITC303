df4d4f19793b346641930d1e0f348cd8a9df4f68
Lilitheum <lilith.mcgoldrick@gmail.com>
2025-09-01 10:23:17 +1000

    Monitoring Script V1
    Mostly works, still stuff that needs to be added and there are 100% bugs that I don't know about yet.

--- MonitoringScript/oversight_monitoring/script_json.py
+++ MonitoringScript/oversight_monitoring/script_json.py
@@ -4,46 +4,132 @@ import time
 import sys
 import websockets
 import asyncio
+import datetime
+import platform
 
-procs = None
+"""Note: More work has to be done on this, but it seems to mostly work."""
+
+processCache = {}
+serviceCache = {}
+disk = None
+cpu = None
+ram = None
+
+deviceDetails = platform.uname()
+
+data = {
+    'timestamp': None,
+    'device': {
+        'system': deviceDetails[0],
+        'deviceName': deviceDetails[1],
+        'release': deviceDetails[2],
+        'version': deviceDetails[3],
+        'machine': deviceDetails[4],
+        'processor': deviceDetails[5]
+    },
+    'processes': {},
+    'services': {},
+    'disk': {},
+    'cpu': {
+        'frequency': 0,
+        'cores': [],
+    },
+    'ram': {
+        'totalBytes': 0,
+        'availableBytes': 0,
+        'percentUsed': 0,
+    }
+}
 
 print(sys.executable)
 
 async def main(uri):
+    global data
+    print(platform.uname()[0])
+
     async with websockets.connect(uri) as websocket:
         
-        """Test stuff to try and see if I can get a response"""
         await websocket.send('Hello World!')
         response = await websocket.recv()
         print(response)
-        
-        await send_report(websocket)
+    
+        await createData()
+        await websocket.send(json.dumps(data))
         while True:
             time.sleep(5)
-            await send_report(websocket)
+            await createData()
+            await websocket.send(json.dumps(data))
 
+async def updateProcesses():
+    global processCache
+
+    for proc in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent', 'io_counters', 'status']):
+        if proc.pid not in processCache:
+            processCache[proc.pid] = proc
+            try:
+                proc.cpu_percent(None)
+            except psutil.NoSuchProcess:
+                pass
     
-async def send_report(websocket):
-    data = [
-        
-    ]
+    snapshot = {}
+    for pid, proc in list(processCache.items()):
+        try:
+            snapshot.update({str(proc.pid): {
+                'name': str(proc.name()),
+                'cpu_percent': str(proc.cpu_percent()),
+                'memory_percent': str(proc.memory_percent()),
+                'io_counters': str(proc.io_counters()),
+                'status': str(proc.status())
+            }})
+        except psutil.NoSuchProcess:
+            processCache.pop(pid, None)
     
-    procs = psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent', 'io_counters', 'status'])
+    return snapshot
+
+async def updateServices():
+    global serviceCache
+
+    for serv in psutil.win_service_iter():
+        if serv.pid not in serviceCache:
+            serviceCache[serv.pid] = serv
     
-    for proc in procs:
-        data.append(
-            {'pid': str(proc.pid), 
-            'name': str(proc.name()),
-            'cpu_percent': str(proc.cpu_percent()),
-            'memory_percent': str(proc.memory_percent()),
-            'io_counters': str(proc.io_counters()),
-            'status': str(proc.status())}
-        )
+    snapshot = {}
+    for pid, serv in list(serviceCache.items()):
+        try:
+            snapshot.update({str(serv.pid()): {
+                'name': str(serv.name()),
+                'display_name': str(serv.display_name()),
+                'start_type': str(serv.start_type()),
+                'status': str(serv.status())
+            }})
+        except psutil.NoSuchProcess:
+            serviceCache.pop(pid, None)
     
-    """More test prints"""
-    print('Trying send...')
-    await websocket.send(json.dumps(data))
-    print('Sent!')
+    return snapshot
+
+"""WIP Segment"""
+async def updateDisk():
+    diskDict = {}
+    diskInfo = psutil.disk_io_counters(perdisk=True)
+    """for disk in iter(diskInfo):
+        diskDict.update({str({disk}): {
+            'readCount': disk[0]
+        }})"""
+    return diskDict
+
+async def createData():
+    global data
+    data['timestamp'] = str(datetime.datetime.now())
+    procecessSnapshot = updateProcesses()
+    data['processes'] = await procecessSnapshot
+    serviceSnapshot = updateServices()
+    data['services'] = await serviceSnapshot
+    disks = psutil.disk_io_counters(perdisk=True)
+    data['disk'] = disks
+    virtualMemory = psutil.virtual_memory()
+    data['ram']['totalBytes'] = virtualMemory.total
+    data['ram']['availableBytes'] = virtualMemory.available
+    data['ram']['percentUsed'] = virtualMemory.percent
 
 if __name__ == "__main__":
     websocket_uri = "ws://localhost:3000/api/ws"
